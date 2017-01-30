5ede5f2a092271addfb50c7297d745dd0ed1f6dc
Dane Lowrey <dane.r.lowrey@gmail.com>
2017-01-29 22:46:41 -0600

    Added detailed comments (except GUI)

--- SudokuSolver/Board.py
+++ SudokuSolver/Board.py
@@ -3,46 +3,74 @@ from SudokuSolver import Cell
 
 class Board:
     def __init__(self):
+        # fill a 9x9 matrix with empty cells
         self.board = [[Cell.Cell(r, c) for c in range(9)] for r in range(9)]
 
-
+    # set a cell's number (used in GUI because Board.py object cannot be indexed)
     def set_number(self, number, row, col):
         self.board[row][col].set_number(number)
 
+    # get a cell's number (used in GUI because Board.py object cannot be indexed)
     def get_number(self, row, col):
         return self.board[row][col].get_number()
 
+    # clear the entire board (all cell.number set to 0)
     def clear(self):
         for r in range(9):
             for c in range(9):
                 self.board[r][c].set_number(0)
 
+    # find all possible numbers that could be in a cell without
+    # breaking sudoku rules
     def __find_possible(self, row, col):
         effected_row = []
         effected_col = []
         effected_box = []
+
+        # Find numbers existing in the cell's effected column and row
         for i in range(9):
             if i != row:
                 effected_col.append(self.board[i][col].get_number())
             if i != col:
                 effected_row.append(self.board[row][i].get_number())
 
-        # Written by Orion Koepke to check a cell's block
+        # Written by Orion Koepke to check a cell's effected block
         for r in range(row - (row % 3), row + (3 - (row % 3))):
             for c in range(col - (col % 3), col + (3 - (col % 3))):
                 if c != col and r != row:
                     effected_box.append(self.board[r][c].get_number())
-        solution = []
+
+        # solution = []
         # for x in range(9):
         #     x += 1
         #     if x not in effected_row and x not in effected_col and x not in effected_box:
         #         solution.append(x)
+        # return solution
 
+        # return a list containing numbers 1 through 9 that do not appear in
+        # any of the effected_xxx lists
         return [i + 1 for i in range(9) if i + 1 not in effected_row\
                 and i + 1 not in effected_col\
                 and i + 1 not in effected_box]
 
 
+    # solve(self)
+    # Brute force backtracking algorithm O(n^2)
+    # For each cell the algorithm:
+    #   1) See if cell is blank (i.e cell.number == 0)
+    #       2) find all possible numbers that could go in the cell (see self.__find_possible(row, col))
+    #       3) if there is a possible number that could fill the cell,
+    #           3a. put the number in the cell and put the cell in 'cells_tried' (a list of cell's that we have filled already)
+    #           3b. advance "col"
+    #       4) else if cells_tried is not empty
+    #           4a. reset the current cell's number index (because it could potentially get new "possible_numbers" when we reach it again
+    #           4b. get the last cell out of cells_tried and set clear it's number (i.e. set it to 0)
+    #           4c. remove the cell obtained in 4b from cells_tried
+    #           4d. set the loop back to the previous cell obtained in 4b and try it's next possible number
+    #               4d comment: trying the next number for the cell obtained in 4b will just get the same possible_numbers but
+    #                           this time the cell's number_idx will be +1 of the previous time we tried to fit it
+    #       5) else the puzzle is unsolvable
+    #   6) else advance col
     def solve(self):
         cells_tried = []
         row = 0
--- SudokuSolver/Cell.py
+++ SudokuSolver/Cell.py
@@ -1,21 +1,30 @@
 class Cell:
     def __init__(self, row, col):
+        # the cell's position on the board
         self.row = row
         self.col = col
+        # the cell's starting number (0 is 'empty')
         self.number = 0
+        # list of numbers (1 - 9) that could potentially be
+        # the cell's number without breaking sudoku rules
         self.possible_numbers = []
-        self.numbers_tried = []
+        # The indexer that is used to advance through
+        # each of this cell's possible_numbers (will be reset if the cell is backtracked over)
         self.number_idx = -1
 
+    # get the number of this cell
     def get_number(self):
         return self.number
 
+    # set the number of this cell
     def set_number(self, number):
         self.number = number
 
+    # check if this cell has a possible number
     def has_possible_number(self):
         return self.number_idx < (len(self.possible_numbers) - 1)
 
+    # try the next possible number in this cell
     def try_next_number(self):
         self.number_idx += 1
         self.number = self.possible_numbers[self.number_idx]
