3578c261c0bbedd1fbc0d43d8955a143a817a666
dlowrey <dane.r.lowrey@gmail.com>
2017-04-20 17:11:56 -0500

    Updated run.py with comments

--- Solver/run.py
+++ Solver/run.py
@@ -5,8 +5,8 @@ MARGIN = 20  # Pixels around the board
 SIDE = 50  # Width of every board cell.
 WIDTH = HEIGHT = MARGIN * 2 + SIDE * 9  # Width and height of the whole board
 
-board = SudokuBoard()
-parent = Tk()
-GUI(parent, board)
+board = SudokuBoard() # New SudokuBoard object
+parent = Tk()   # New TKinter window
+GUI(parent, board) # New GUI with the Tkinter window and board
 parent.geometry("%dx%d" % (WIDTH, HEIGHT + 40))
-parent.mainloop()
+parent.mainloop()   # Start the GUI
