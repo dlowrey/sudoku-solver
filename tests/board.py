77645cb59b76707bf7f158d101425245cbef1be8
dlowrey <dane.r.lowrey@gmail.com>
2017-11-09 17:45:40 -0600

    Board tests

--- tests/board.py
+++ tests/board.py
@@ -1,5 +1,6 @@
 from src.sudoku import Board, Cell
 
+# Test creation of board object
 board = Board()
 passed = type(board) == Board
 print("create Board object success: {}".format(passed))
@@ -11,3 +12,17 @@ for r in range(9):
         cell = board.board[r][c]
         passed = passed and type(cell) == Cell
 print("board made of Cell objects: {}".format(passed))
+
+# Test the boards validation method
+valid = board.is_valid()
+passed = valid
+print("validate empty board success: {}".format(passed))
+
+# Make the board invalid and test validation method
+cell_1 = board.board[0][0]
+cell_1.number = 1
+cell_2 = board.board[0][1]
+cell_2.number = 1
+valid = board.is_valid()
+passed = not valid
+print("validate invalid board success: {}".format(passed))
